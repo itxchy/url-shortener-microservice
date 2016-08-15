9eb2e2d100a42de1643a845a6b0e5ea1ca2e72ed
Matt Trifilo <matt.trifilo@gmail.com>
2016-08-15 14:03:39 -0500

    separate redirect and save operations into new functions

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ db.once('open', function() {
 
 app.use(compression());
 
-app.use('/:url', (req, res) => {
+app.use('/:url', (req, res, next) => {
 
     // ignore requests for /favicon.ico
     if (req.url === '/favicon.ico') {
@@ -36,25 +36,39 @@ app.use('/:url', (req, res) => {
     var validUrl = validator.isUrl(url);
     var errorResponse = { error: url + " is not valid."};
 
-    // TODO break up the logic into smaller, single-responsibily, testable functions
-
     if (!validUrl) {
 
         // if the passed param is invalid, check if it's a short URL id.
         // if so, then redirect to the original URL. if not, then return an error.
-        ShortUrl.findOne({'id': url}, 'original_url', function(err, shortUrl) {
-            if (err) {
-                return res.status(400).send(errorResponse);                
-            }
-            return res.redirect(shortUrl.original_url); 
-        });
+        redirectToOriginalUrl(res, errorResponse);
 
         return;
 
     }
 
+    next();
+
+}, function (req, res, next) {
+
     var generatedID = shortid.generate();
     var shortendURL = `${req.hostname}/${generatedID}`
+
+    saveNewShortUrl(generatedID, shortendURL);
+
+});
+
+function redirectToOriginalUrl(res, errorResponse) {
+
+    ShortUrl.findOne({'id': url}, 'original_url', function(err, shortUrl) {
+        if (err) {
+            return res.status(400).send(errorResponse);                
+        }
+        return res.redirect(shortUrl.original_url); 
+    });    
+
+}
+
+function saveNewShortUrl(generatedID, shortendURL) {
 
     var newShortUrl = new ShortUrl({
         id: generatedID,
@@ -67,7 +81,9 @@ app.use('/:url', (req, res) => {
         console.log('New short URL saved!');
     });
 
-});
+    return;
+
+}
 
 var server = app.listen(process.env.PORT || 4000, () => {
     console.log('Express app listening!');
